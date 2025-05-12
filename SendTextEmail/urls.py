08b19a1aac677150be3bfee6234892b137dd45af
Paras Munoli <parasmunoli@gmail.com>
2025-05-12 14:29:56 +0530

    Added route to send text email

--- SendTextEmail/urls.py
+++ SendTextEmail/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from views import send_text_email
-
-path('sendTextEmail/', views.sendTextEmail, name='sendTextEmail')
+urlpatterns = [
+    path('sendTextEmail/', views.sendTextEmail, name='sendTextEmail'),
+]
