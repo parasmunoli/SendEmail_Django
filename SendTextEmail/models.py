d239c0329a4d4513c5a2c957568059deaf5ef659
Paras Munoli <parasmunoli@gmail.com>
2025-05-14 19:39:12 +0530

    Modified User model

--- SendTextEmail/models.py
+++ SendTextEmail/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class UserRegistration(models.Model):
+    userID = models.AutoField(primary_key=True)
     username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, max_length=255)
+    phone = models.CharField(max_length=15)
+    gender = models.CharField(max_length=10)
     password = models.CharField(max_length=255)
 
     def __str__(self):
